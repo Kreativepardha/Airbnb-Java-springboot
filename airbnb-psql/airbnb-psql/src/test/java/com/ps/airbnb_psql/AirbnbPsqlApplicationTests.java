51ffbac8f632be87f5c35da27f5bc23411aa9d6d
Kreativepardha <saradhipardha12@gmail.com>
2025-01-18 14:01:43 +0530

    postgres version

---
+++ airbnb-psql/airbnb-psql/src/test/java/com/ps/airbnb_psql/AirbnbPsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.airbnb_psql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbPsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
