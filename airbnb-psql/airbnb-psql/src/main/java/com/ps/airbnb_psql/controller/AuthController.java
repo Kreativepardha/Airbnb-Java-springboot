d5b7f62d5a151214d6ed1cf6ebb675f792c360d5
Kreativepardha <saradhipardha12@gmail.com>
2025-01-20 18:36:16 +0530

    ;

--- airbnb-psql/airbnb-psql/src/main/java/com/ps/airbnb_psql/controller/AuthController.java
+++ airbnb-psql/airbnb-psql/src/main/java/com/ps/airbnb_psql/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.ps.airbnb_psql.controller;
 
 
+import com.ps.airbnb_psql.dto.LoginRequest;
 import com.ps.airbnb_psql.dto.Response;
 import com.ps.airbnb_psql.entity.User;
 import com.ps.airbnb_psql.service.interfac.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +19,16 @@ public class AuthController {
     @Autowired
     private IUserService userService;
 
+    @PostMapping("/register")
     public ResponseEntity<Response> register(@RequestBody User user) {
         Response res = userService.register(user);
         return ResponseEntity.status(res.getStatusCode()).body(res);
     }
 
-
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response res = userService.login(loginRequest);
+        return ResponseEntity.status(res.getStatusCode()).body(res);
+    }
 
 
 }
